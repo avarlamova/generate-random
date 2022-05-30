e030497b596f4177805f9a80f0f7aff5f94f5058
avarlamova <59831804+avarlamova@users.noreply.github.com>
2022-05-30 22:16:09 +0300

    types added

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,9 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    randomNames: [],
-    randomPrices: [],
-    randomAmounts: [],
+    randomNames: [] as string[],
+    randomPrices: [] as number[],
+    randomAmounts: [] as number[],
   },
   getters: {
     getTableVisibility(state) {
@@ -15,32 +15,32 @@ export default createStore({
       );
     },
     getRandomNames(state) {
-      return state.randomNames;
+      return state.randomNames as string[];
     },
     getRandomPrices(state) {
-      return state.randomPrices;
+      return state.randomPrices as number[];
     },
     getRandomAmounts(state) {
-      return state.randomAmounts;
+      return state.randomAmounts as number[];
     },
   },
   mutations: {
     randomizeData(state) {
       for (let i = 0; i < 51; i++) {
         //generate names
-        const wordLength = 5;
-        const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        const wordLength: number = 5;
+        const alphabet: string = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
         let word = "";
         for (let j = 0; j < wordLength; j++)
           word += alphabet[Math.floor(Math.random() * alphabet.length)];
         state.randomNames.push(word);
 
         //generate price
-        const price = Math.random() * (1000 - 0.01) + 0.01; // случайное число от 0.01 до 1000.00
+        const price: number = Math.random() * (1000 - 0.01) + 0.01; // случайное число от 0.01 до 1000.00
         state.randomPrices.push(Number(price.toFixed(2)));
 
         //generate amounts
-        const amount = Math.ceil(Math.random() * 100); //случайное число от 1 до 100
+        const amount: number = Math.ceil(Math.random() * 100); //случайное число от 1 до 100
         state.randomAmounts.push(amount);
       }
     },
